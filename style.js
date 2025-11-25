9267b9a77ba32687cdb4e95eb1136116263a37a0
Owen Pauptit <owenpauptit@gmail.com>
2025-11-25 18:21:06 +0000

    Simplified website to one page

--- style.js
+++ style.js
@@ -11,3 +11,17 @@ changeColourMode = function() {
         document.documentElement.style.setProperty('--bgcol', light);
     }
 }
+
+// window.onload <- function() {
+//     console.log("Hello")
+//     console.log(window.innerWidth)
+//     if (window.innerWidth < 1000) {
+//         console.log("On mobile!")
+//         document.documentElement.style.setProperty('--smalltext', '36pt');
+//         document.documentElement.style.setProperty('--mediumtext', '72pt');
+//         document.documentElement.style.setProperty('--largetext', '162pt');
+//     } else {
+//         console.log("Not on mobile!")
+//     }
+// }
+
